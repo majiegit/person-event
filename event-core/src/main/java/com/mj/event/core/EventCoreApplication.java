4dedc484392162d23a192209208bf0bde27211b0
gefeichao <5710538+tian_jing_admin@user.noreply.gitee.com>
2020-06-08 14:57:34 +0800

    Project init

---
+++ event-core/src/main/java/com/mj/event/core/EventCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mj.event.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventCoreApplication.class, args);
+    }
+
+}
